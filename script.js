baa99ea5d1596743c4cdcd6f76e64965b8824ff5
COMPILELINE <95524954+COMPILELINE@users.noreply.github.com>
2022-12-22 22:45:38 +0530

    init

---
+++ script.js
@@ -0,0 +1,67 @@
+document.querySelector('#one').addEventListener('click' , () => {
+    document.forms.display.value += 1;
+})
+
+document.querySelector('#two').addEventListener('click' , () => {
+    document.forms.display.value +=2;
+})
+
+document.querySelector('#three').addEventListener('click' , () => {
+    document.forms.display.value += 3;
+})
+
+document.querySelector('#four').addEventListener('click' , () => {
+    document.forms.display.value += 4;
+})
+
+document.querySelector('#five').addEventListener('click' , () => {
+    document.forms.display.value += 5;
+})
+
+document.querySelector('#six').addEventListener('click' , () => {
+    document.forms.display.value += 6;
+})
+
+document.querySelector('#seven').addEventListener('click' , () => {
+    document.forms.display.value += 7;
+})
+
+document.querySelector('#eight').addEventListener('click' , () => {
+    document.forms.display.value += 8;
+})
+
+document.querySelector('#nine').addEventListener('click' , () => {
+    document.forms.display.value += 9;
+})
+
+document.querySelector('#zero').addEventListener('click' , () => {
+    document.forms.display.value += 0;
+})
+
+document.querySelector('#divide').addEventListener('click' , () => {
+    document.forms.display.value += '/';
+})
+
+document.querySelector('#multi').addEventListener('click' , () => {
+    document.forms.display.value += '*';
+})
+
+document.querySelector('#subs').addEventListener('click' , () => {
+    document.forms.display.value += '-';
+})
+
+document.querySelector('#add').addEventListener('click' , () => {
+    document.forms.display.value += '+';
+})
+
+document.querySelector('#equal').addEventListener('click' , () => {
+    if(document.forms.display.value == ""){
+        return ;
+    } else {
+        document.forms.display.value = eval(document.forms.display.value)
+    }
+})
+
+document.querySelector('#clear').addEventListener('click' , () => {
+    document.forms.display.value = "";
+})
